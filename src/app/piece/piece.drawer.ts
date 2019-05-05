741efce847ab52ca071c4028c23168b370a24401
kubiakba <kubiakb1@gmail.com>
2019-05-05 16:47:17 +0200

    clean ups

--- src/app/piece/piece.drawer.ts
+++ src/app/piece/piece.drawer.ts
@@ -2,18 +2,12 @@ import {PiecePosition, PieceType, Player} from './piece';
 import {Renderer2} from '@angular/core';
 
 export function drawPieceOnBoard(piece: PiecePosition, renderer: Renderer2, element: any) {
-  if (!!!piece.piece) {
-    console.log('That\'s it dude.');
-  }
   const player: string = Player[piece.piece.owner].toLowerCase();
   const type: string = PieceType[piece.piece.type].toLowerCase();
   renderer.addClass(element, `square-${player}-${type}`);
 }
 
 export function removePieceFromBoard(piece: PiecePosition, renderer: Renderer2, element: any) {
-  if (!!!piece.piece) {
-    console.log('That\'s it dude.');
-  }
   const player: string = Player[piece.piece.owner].toLowerCase();
   const type: string = PieceType[piece.piece.type].toLowerCase();
   renderer.removeClass(element, `square-${player}-${type}`);
